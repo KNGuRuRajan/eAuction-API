b135511a8bb5e8429121ae7e15909a75a7e27227
Guru <gurufirebase2022@gmail.com>
2022-06-08 15:35:13 +0530

    Add project files.

---
+++ EAuction.APIGateway/Repositories/UserRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EAuction.APIGateway.Data.Abstractions;
+using EAuction.APIGateway.Models;
+using EAuction.APIGateway.Repositories.Abstractions;
+using MongoDB.Driver;
+
+namespace EAuction.APIGateway.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IUserContext _context;
+
+        public UserRepository(IUserContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            var result = await _context.Users.Find(p => true).ToListAsync();
+            return result;
+        }
+
+        public async Task<User> GetUser(string emailId, string password)
+        {
+            if (string.IsNullOrEmpty(emailId) || string.IsNullOrEmpty(password))
+            {
+                throw new Exception("Enter valid credentials...");
+            }
+            var result = await _context.Users.Find(p => p.Email.ToLower() == emailId.ToLower() && p.Password == password).FirstOrDefaultAsync();
+            return result;
+        }       
+
+        public async Task Create(User user)
+        {
+            var result = await _context.Users.Find(p => p.Email.ToLower() == user.Email.ToLower()).FirstOrDefaultAsync();
+            if (result == null)
+            {
+                await _context.Users.InsertOneAsync(user);
+            }
+            else
+            {
+                throw new Exception("User already exist...");
+            }
+        }
+    }
+}
---
+++ EAuction.Order.Application/Queries/GetBidsByProductIdQuery.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using EAuction.Order.Application.Responses;
+using MediatR;
+
+namespace EAuction.Order.Application.Queries
+{
+    public class GetBidsByProductIdQuery : IRequest<IEnumerable<BidResponse>>
+    {
+        public GetBidsByProductIdQuery(string productId)
+        {
+            ProductId = productId;
+        }
+
+        public string ProductId { get; set; }
+
+    }
+}
